56b99c164e8dbcc611ddad909b7c672561729573
Ryan Howe <ryanwhowe@gmail.com>
2025-11-17 11:10:13 -0500

    config and alias updates

--- oh-my-zsh/aliasez.zsh
+++ oh-my-zsh/aliasez.zsh
@@ -4,10 +4,10 @@ alias bat="batcat"
 
 alias pad="code ~/ScratchPad.md"
 
-alias ls="exa"
-alias ll="ls -l"
+alias ls="exa --icons --group-directories-first"
+alias ll="ls -l -g -h"
 alias la="ls -a"
-alias lla="ls -l -a"
+alias lla="ls -l -a -g -h"
 
 alias fortunes="echo \"\" && fortune rules DS9 TNG VOY clonewars startrek && echo \"\""
 
